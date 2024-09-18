e7156ac984302039776c088057b08185643df01b
Egglol1 <24egglal@gmail.com>
2024-09-18 12:33:50 -0400

    Changed syntax of list to be more legible

--- js/script.js
+++ js/script.js
@@ -1,22 +1,8 @@
-pokemonList = [{
-    name: 'Bulbasaur',
-    height: 0.7,
-    weight: 6.9,
-    types: ['Grass', 'Poison']
-},
-{
-    name: 'Charmander',
-    height: 0.6,
-    weight: 8.5,
-    types: ['Fire']
-},
-{
-    name: 'Squirtle',
-    height: 0.5,
-    weight: 9.0,
-    types: ['Water']
-}];
+pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['Grass', 'Poison']},
+    {name: 'Charmander', height: 0.6, weight: 8.5, types: ['Fire']},
+    {name: 'Squirtle', height: 0.5, weight: 9.0, types: ['Water']}];
 
-for (let i = 0; i < length(pokemonList); i++) {
-    document.write(pokemonList(i))
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (Height:' + pokemonList[i].height + ") ")
 }
