0bab06bf29bd536bfea98565cf572cce17263048
Egglol1 <24egglal@gmail.com>
2024-09-19 10:58:18 -0400

    Altered some lines to follow best practices

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 pokemonList = [
     {name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['Grass', 'Poison']},
     {name: 'Charmander', height: 0.6, weight: 8.5, types: ['Fire']},
-    {name: 'Squirtle', height: 0.5, weight: 9.0, types: ['Water']}];
+    {name: 'Squirtle', height: 0.5, weight: 9.0, types: ['Water']}
+];
 
 for (let i = 0; i < pokemonList.length; i++) {
     document.write(pokemonList[i].name + ' (Height:' + pokemonList[i].height + ") ")
