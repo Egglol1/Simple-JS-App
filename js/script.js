9976940bd974c5504d09367e6e7a9b46d2bd410b
Egglol1 <24egglal@gmail.com>
2024-09-19 11:11:57 -0400

    Converted printArrayDetails to a funtion to work with later array additions

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-pokemonList = [
+let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['Grass', 'Poison']},
     {name: 'Charmander', height: 0.6, weight: 8.5, types: ['Fire']},
     {name: 'Squirtle', height: 0.5, weight: 9.0, types: ['Water']}
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' (Height:' + pokemonList[i].height + ") ")
-    if (pokemonList[i].height >= 0.7) {
-        document.write(' - Wow! That\'s Tall! ')
-    }  
+function printArrayDetails(list){
+    for (let i = 0; i < list.length; i++) {
+        document.write(list[i].name + ' (Height:' + list[i].height + ") ")
+        if (list[i].height >= 0.7) {
+            document.write(' - Wow! That\'s Tall! ')
+        }  
+    }
 }
+
+printArrayDetails(pokemonList);
