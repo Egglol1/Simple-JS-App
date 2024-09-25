b3f70bafd64a9c9b784b17d5f21ca4a635bd8184
Egglol1 <24egglal@gmail.com>
2024-09-25 12:24:14 -0400

    Contained pokemonList within and IIFE

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['Grass', 'Poison']},
-    {name: 'Charmander', height: 0.6, weight: 8.5, types: ['Fire']},
-    {name: 'Squirtle', height: 0.5, weight: 9.0, types: ['Water']}
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['Grass', 'Poison']},
+        {name: 'Charmander', height: 0.6, weight: 8.5, types: ['Fire']},
+        {name: 'Squirtle', height: 0.5, weight: 9.0, types: ['Water']}
+    ];
 
-function printArrayDetails(list){
-    for (let i = 0; i < list.length; i++) {
-        document.write(list[i].name + ' (Height:' + list[i].height + ") ")
-        if (list[i].height >= 0.7) {
-            document.write(' - Wow! That\'s Tall! ')
-        }  
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-}
 
-printArrayDetails(pokemonList);
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(item) {
+    document.write(item.name + ' (Height:' + item.height + ") ")
+    if (item.height >= 0.7) {
+        document.write(' - Wow! That\'s Big!')
+    }
+});
