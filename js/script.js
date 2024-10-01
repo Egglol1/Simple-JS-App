59d99876d81cc34c3df0b4fdfccbf73290c4c2d3
Egglol1 <24egglal@gmail.com>
2024-10-01 10:46:44 -0400

    Replaced pokemonList with an empty array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 let pokemonRepository = (function() {
-    let pokemonList = [
-        {name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['Grass', 'Poison']},
-        {name: 'Charmander', height: 0.6, weight: 8.5, types: ['Fire']},
-        {name: 'Squirtle', height: 0.5, weight: 9.0, types: ['Water']}
-    ];
+    let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function loadList() {
